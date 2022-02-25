369a7ac7d9a0d6356aa79e1d828d6831d332db96
vosnemo <nemocastillo@hotmail.com>
2022-02-25 11:47:16 -0600

    WIP: Level Screen

---
+++ lib/src/ui/themes/theme.dart
@@ -0,0 +1,21 @@
+import 'package:dartmission/src/ui/themes/typo.dart';
+import 'package:flutter/material.dart';
+
+ThemeData dartMissionTheme = ThemeData(
+  textTheme: dartMissionTextTheme,
+  // colorScheme: const ColorScheme(
+  //   background: aliceBluePrimary,
+  //   brightness: Brightness.light,
+  //   error: error600,
+  //   onBackground: glaucous500,
+  //   onError: aliceBluePrimary,
+  //   onPrimary: aliceBluePrimary,
+  //   onSecondary: indigoDye500,
+  //   onSurface: glaucous500,
+  //   primary: viridianGreen500,
+  //   primaryVariant: viridianGreen100,
+  //   secondary: yellowOrange500,
+  //   secondaryVariant: yellowOrange200,
+  //   surface: aliceBluePrimary,
+  // ),
+);
