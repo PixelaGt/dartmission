c80f99588a2f3570eec315e3c442882ef464ca24
Pablo Bautista <pablo@pixela.tech>
2022-03-11 16:20:07 -0600

    add: url strategy

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,8 @@
 import 'package:dartmission/bootstrap.dart';
 import 'package:dartmission/dartmission_app.dart';
+import 'package:dartmission/strategy/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   bootstrap(() => const DartmissionApp());
 }
